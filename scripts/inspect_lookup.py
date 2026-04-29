5caef1e1053fa3c7bc6065d7f1352c620f2cfdaa
Toasted Potato00 <kennycornellius09@gmail.com>
2026-04-29 16:33:47 +0700

    commit 3

---
+++ scripts/inspect_lookup.py
@@ -0,0 +1,6 @@
+﻿import polars as pl
+df = pl.read_parquet('data/bars/_market_lookup.parquet')
+print(f'Lookup rows: {len(df)}')
+print(f'Schema: {df.schema}')
+print(f'Null created_at: {df["created_at"].null_count()}')
+print(df.head())
